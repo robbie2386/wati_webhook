8865a6bdf8c60098d84b9c8e4ecfcff532d74a6d
robbie2386 <robbieasnur86@gmail.com>
2025-03-08 02:56:07 +0700

    Add home route to fix 404 issue

--- app.py
+++ app.py
@@ -60,6 +60,18 @@ def whatsapp_webhook():
 
     return jsonify({"status": "Processed"}), 200
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Server is running!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
 
